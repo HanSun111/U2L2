211c89a3f5628c362cf05f3c709a8426707b2a09
BT_4N2_14 <hsun7920@bths.edu>
2022-09-28 11:45:41 -0400

    poem problem

--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -1,20 +1,6 @@
 import java.util.Scanner;
 public class U2L2Runner {
-    public static void main(String[] args) {
-        Scanner construction = new Scanner(System.in);
-        System.out.println("Number of lumber boards needed: ");
-        int boardNum = construction.nextInt();
+    Poem poem = new Poem("Adrian");
+    poem.printPoem();
 
-        System.out.println("Number of windows needed: ");
-        int windowNum = construction.nextInt();
-
-        System.out.println("Tax rate in your area: ");
-        double taxEvasion = construction.nextDouble();
-
-        ConstructionPricer price = new ConstructionPricer(5.53 , 50.59 , taxEvasion);
-        System.out.println("Material cost: " + String.format("%.2f", price.materialsCost(boardNum , windowNum)));
-        System.out.println("Total cost with taxes because taxes are cool: " + String.format("%.2f", price.totalWithTax(price.materialsCost(boardNum , windowNum))));
-
-
-    }
 }
